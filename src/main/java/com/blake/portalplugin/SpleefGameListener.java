2090eda76570b0573cf3f27e1d8b59cd191f284c
BlakeOReilly <blakeoreillywork@gmail.com>
2025-11-02 11:13:18 +1300

    Automated: build success

--- src/main/java/com/blake/portalplugin/SpleefGameListener.java
+++ src/main/java/com/blake/portalplugin/SpleefGameListener.java
@@ -1,15 +1,23 @@
 package com.blake.portalplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
 
 public class SpleefGameListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        // Additional logic for when a player joins
+        Scoreboard scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+        Objective objective = scoreboard.registerNewObjective("spleef", "dummy", ChatColor.GREEN + "Spleef Score");
+        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+        player.setScoreboard(scoreboard);
     }
 }
