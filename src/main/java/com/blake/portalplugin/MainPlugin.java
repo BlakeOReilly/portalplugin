cca80b80b66ee1eeab52a64037e0a562ef636db5
BlakeOReilly <blakeoreillywork@gmail.com>
2025-11-02 13:55:20 +1300

    Automated: build success

--- src/main/java/com/blake/portalplugin/MainPlugin.java
+++ src/main/java/com/blake/portalplugin/MainPlugin.java
@@ -3,10 +3,9 @@ package com.blake.portalplugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MainPlugin extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        getCommand("gamestate").setExecutor(new GameStateCommand());
+        getCommand("gamestate").setExecutor(new GameStateCommand(this));
     }
 
     @Override
